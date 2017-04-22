9590a9d17e3312a7bc59215832955d093b0d48cf
FranFuentesA <fran_fuentes20@hotmail.com>
2017-04-22 22:43:13 +0200

    add Carpetas

--- app/src/main/java/com/example/fran/aplicacioncliente/Test.java
+++ app/src/main/java/com/example/fran/aplicacioncliente/Test.java
@@ -20,7 +20,7 @@ public class Test extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.test2);
 
 
 
